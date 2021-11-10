0713c58a55af4c7fcc076306e2ee8a2d3755b80e
liudraven <402072123@qq.com>
2021-11-10 23:00:51 +0800

    20211110 动态参数

---
+++ 20211110.py
@@ -0,0 +1,16 @@
+# -*- coding:UTF-8 -*-
+# auther:drliu
+# aim: 
+
+def main(a, *args, **kwargs):
+    print(a)
+    for item in args:
+        print(item)
+
+    for key, value in kwargs.items():
+        print(key, value)
+
+
+
+if __name__ == '__main__':
+    main('a', 'a','b', read_flag=1, read_num=2)
