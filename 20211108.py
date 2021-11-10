473021f7a66252aa38b15eed9119edd2c0b313b5
liudraven <402072123@qq.com>
2021-11-10 22:11:17 +0800

    20211110 装饰器2

---
+++ 20211108.py
@@ -0,0 +1,10 @@
+# -*- coding:UTF-8 -*-
+# auther:drliu
+# aim: 
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
--- lib/20201107.py
+++ lib/20201107.py
@@ -10,27 +10,30 @@ def log_decorater(fuc):
     :param fuc: raw function
     :return: new function
     """
-    def decorator():
+    def decorator(*args, **kwargs):
         print('INFO::', 'will do function:{0}'.format(fuc.__name__))
         a = time.perf_counter()
         print('INFO::', 'function:{0} begin time:{1}'.format(fuc.__name__, time.strftime('%Y-%m-%d %H:%M:%S')))
 
-        fuc()
+        ret = fuc(*args, **kwargs)
 
         print('INFO::', 'function:{0} completed'.format(fuc.__name__))
         b = time.perf_counter()
         print('INFO::', 'function:{0} end time:{1}'.format(fuc.__name__, time.strftime('%Y-%m-%d %H:%M:%S')))
         cost_time = b - a
         print('INFO::', 'function:{0} cost time:{1} s'.format(fuc.__name__, cost_time))
+
+        return ret
     return decorator
 
 
 @log_decorater
-def main():
+def main(num=1):
     time.sleep(3)
+    return num
 
 
 if __name__ == '__main__':
-    main()
+    print(main())
 
 
