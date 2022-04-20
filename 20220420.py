516efe12821c1324466e218439668a526dba1c49
liudraven <402072123@qq.com>
2022-04-21 00:05:37 +0800

    20220306 测试父类，子类的属性关系

--- 20220420.py
+++ 20220420.py
@@ -6,8 +6,8 @@
 class Father(object):
     def __init__(self):
         self.name = 0
-        self.__name = 1
-        self._age = 2
+        self._age = 1
+        self.__id = 2
         pass
 
     def father_function(self):
@@ -17,6 +17,11 @@ class Father(object):
         pass
 
     def _father_function(self):
+        print(self.name)
+        print(__name__)
+        pass
+
+    def father_interface(self):
         pass
 
 
@@ -24,15 +29,50 @@ class Children(Father):
     def __init__(self):
         super().__init__()
 
-    def chileren_function(self):
+    def children_function(self):
+        self.father_function()
+        a = self.name
+        b = self._age
+        c = self.__id
+        print(a, b, c)
         pass
 
-    def
+    def children_interfacr(self):
+        self.__father_function1()
+        pass
+
+    def children_interfacr1(self):
+        self._father_function()
+        pass
 
 
 def main():
-    pass
+    father = Father()
+    a = father._age
+    print(a)
+
+    """
+    类中的私有变量 __id
+    类被实例化了后，不能被实例访问
+    不能被子类调用 访问
+    
+    类的私有方法 __function
+    类被实例化后，不能被实例访问
+    不能被子类调用
+    
+    类的保护变量_age
+    类被实例化了后，可以被实例访问,  不建议访问
+    可以被子类调用 访问
+    
+    类的保护方法_father_function
+    可以被子类调用 访问
+    """
+
+
+def main_1():
+    chil = Children()
+    chil._father_function()
 
 
 if __name__ == '__main__':
-    main()
+    main_1()
